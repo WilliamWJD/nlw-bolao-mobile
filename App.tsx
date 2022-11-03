4da64b8918afd0f6239f7583ac5bbc0253503f36
WilliamWJD <william.jose.wjd@gmail.com>
2022-11-03 17:28:29 -0300

    feat: criando authContext

--- App.tsx
+++ App.tsx
@@ -5,18 +5,21 @@ import { SignIn } from './src/pages/SignIn';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
   const [fontsLoading] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {!fontsLoading ? <Loading /> : <SignIn />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {!fontsLoading ? <Loading /> : <SignIn />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
